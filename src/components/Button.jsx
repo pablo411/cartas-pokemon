4bbace6b5bd9a158e83610493b3552e598a8882d
pablo <londro23@hotmail.com>
2024-10-07 08:15:20 +0200

    pokemon

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import "../sass/Button.scss"
+
+const Button = ({icon, handleClick}) => {
+  return (
+    <div className="button__box">
+    <button
+      className="button" 
+      onClick={handleClick}
+     >{icon}
+     </button>
+    <div className="box__shadow"></div>
+    </div>
+  )
+}
+
+export { Button } 

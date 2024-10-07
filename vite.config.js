37644f0ba222cf114e7fd7c527c4074f2101fa33
pablo valderrama <163021604+pablo411@users.noreply.github.com>
2024-10-07 08:20:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: https://github.com/<pablo411>/<cartas-pokemon>base'/<cartas-pokemon>/'
+  https://github.com/<pablo411>/<cartas-pokemon>base'/<cartas-pokemon>/'
   plugins: [react()],
 })

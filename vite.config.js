b3eb8fb98731f58175c46821b5bbe19b18436dc5
pablo <londro23@hotmail.com>
2024-10-08 09:29:58 +0200

    terminado

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  https://github.com/<pablo411>/<cartas-pokemon>base'/<cartas-pokemon>/'
   plugins: [react()],
+  /*
+  base: "https://pablo411.github.io/cartas-pokemon">
+  */
 })
